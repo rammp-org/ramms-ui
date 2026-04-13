17867e64ba428c3f24268172f0ecd38ae77a9bd7
William Emfinger <waemfinger@gmail.com>
2026-04-13 14:34:02 -0500

    fix(status): Fix how bool values are parsed in the status panel

--- Source/RammsUI/Private/UI/RammsStatusPanel.cpp
+++ Source/RammsUI/Private/UI/RammsStatusPanel.cpp
@@ -867,9 +867,20 @@ void URammsStatusPanel::RefreshAllFields()
 					break;
 				}
 				case ERammsStatusValueFormat::Boolean:
-					FormattedValue = FText::FromString(
-						(bIsNumeric ? (NumericValue > 0.5f) : false) ? TEXT("Yes") : TEXT("No"));
+				{
+					if (bIsNumeric)
+					{
+						StringValue = (NumericValue > 0.5f) ? TEXT("Yes") : TEXT("No");
+					}
+					else
+					{
+						StringValue.TrimStartAndEndInline();
+						StringValue.ToLowerInline();
+						bool bVal = StringValue == TEXT("true") || StringValue == TEXT("yes") || StringValue == TEXT("on") || StringValue == TEXT("1");
+						StringValue = bVal ? TEXT("Yes") : TEXT("No");
+					}
 					break;
+				}
 				case ERammsStatusValueFormat::EnumName:
 				{
 					if (Field.EnumType && bIsNumeric)
